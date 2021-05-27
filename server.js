b557acab3b03ad883ffd7320b1ecf1df9d1303ba
João Rosa <joao.junior@itbam.org.br>
2021-05-27 17:50:40 -0400

    feat: multi-message

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var SockJS = require('sockjs-client');
 var Stomp = require('stompjs')
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 connect()
+
+function sendMessage(stompClient){
+    rl.question("Escreva sua mensagem:  ", function(name) {
+        stompClient.send('/topic/qrCode/dev/KA07P9CV00009',{},name);
+        console.log('Mensagem enviada');
+        sendMessage(stompClient)
+    })
+}
 
 function connect(){
     const socket = new SockJS('http://172.100.10.58:8800/socket')
@@ -10,10 +25,14 @@ function connect(){
 
         console.log('Socket is Connected');
 
-        console.log('Listening at \'/topic/qrCode/dev/KA07P9CV00009\'');
+        console.log('Listening at \'/topic/qrCode/dev/KA07P9CV00010\'');
 
-        stompClient.subscribe('/topic/qrCode/dev/KA07P9CV00009',resp =>{
+        stompClient.subscribe('/topic/qrCode/dev/KA07P9CV00010',resp =>{
             console.log('Message: '+ resp.body);
+
+            sendMessage(stompClient)
+    
+            
         })
     })
 }
