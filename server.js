15e9f4e0005dd10c4a6070e39407ebd5425fea64
João Rosa <joao.junior@itbam.org.br>
2021-05-31 16:27:46 -0400

    fix: somethings

--- server.js
+++ server.js
@@ -2,17 +2,31 @@ var SockJS = require('sockjs-client');
 var Stomp = require('stompjs')
 
 const readline = require("readline");
+const { stdout } = require('process');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+const id = 2;
+
+
+var listenUrl;
+var sendUrl;
+
 connect()
 
 function sendMessage(stompClient){
-    rl.question("Escreva sua mensagem:  ", function(name) {
-        stompClient.send('/topic/qrCode/dev/KA07P9CV00009',{},name);
-        console.log('Mensagem enviada');
+    rl.question("", function(name) {
+        stompClient.send(sendUrl,{},`${id}:${name}`);
+        stdout.write(name+'✔');
+        sendMessage(stompClient)
+    })
+}
+
+function setDestinationId(stompClient){
+    rl.question("Informe o id de destino da mensagem: ", function(response) {
+        sendUrl = `/topic/id/${response}`;
         sendMessage(stompClient)
     })
 }
@@ -21,18 +35,19 @@ function connect(){
     const socket = new SockJS('http://172.100.10.58:8800/socket')
     const stompClient = Stomp.over(socket);
 
+    listenUrl = `/topic/id/${id}`;
+
     stompClient.connect({}, frame => {
 
         console.log('Socket is Connected');
 
-        console.log('Listening at \'/topic/qrCode/dev/KA07P9CV00010\'');
+        console.log(`Listening at ${listenUrl}`);
 
-        stompClient.subscribe('/topic/qrCode/dev/KA07P9CV00010',resp =>{
-            console.log('Message: '+ resp.body);
+        setDestinationId(stompClient)
 
+        stompClient.subscribe(listenUrl,resp =>{
+            console.log(`\n ${resp.body}`);
             sendMessage(stompClient)
-    
-            
         })
     })
 }
